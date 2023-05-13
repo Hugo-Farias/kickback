0a4a41705c61ff93666515c3efe07b98a5de7699
Hugo-Farias <hg80rod@gmail.com>
2023-05-13 03:57:46 -0300

    cleanup of unsused code

--- src/background.ts
+++ src/background.ts
@@ -7,39 +7,11 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(
   (details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) => {
     const tabId = details.tabId;
     const url = details.url;
-    if (url && url.includes("kick.com/video")) {
-      const message: UrlChangedMessage = { type: "urlChanged", url: url };
-      // setTimeout(() => chrome.tabs.sendMessage(tabId, message), 5000);
-      chrome.tabs
-        .sendMessage(tabId, message)
-        .then((response) => {
-          console.log("Message sent successfully:", response);
-        })
-        .catch((error) => {
-          console.error("Error sending message:", error);
-        });
-    }
+
+    if (url && !url.includes("kick.com/video")) return;
+
+    const message: UrlChangedMessage = { type: "urlChanged", url: url };
+    // setTimeout(() => chrome.tabs.sendMessage(tabId, message), 5000);
+    chrome.tabs.sendMessage(tabId, message);
   }
 );
-
-// interface UrlChangedMessage {
-//   type: "urlChanged";
-//   url: string;
-// }
-//
-// chrome.tabs.onUpdated.addListener(
-//   (tabId: number, info: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
-//     if (
-//       tab.status === "complete" &&
-//       tab.title !== "Kick" &&
-//       tab.url &&
-//       tab.url.includes("kick.com/video")
-//     ) {
-//       console.log("message");
-//
-//       const message: UrlChangedMessage = { type: "urlChanged", url: tab.url };
-//       // setTimeout(() => chrome.tabs.sendMessage(tabId, message), 5000);
-//       chrome.tabs.sendMessage(tabId, message);
-//     }
-//   }
-// );
--- src/videoObserver.ts
+++ src/videoObserver.ts
@@ -5,7 +5,7 @@ function waitForVideo(callback: (video: HTMLVideoElement) => void) {
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
         if (node instanceof HTMLVideoElement) {
-          // observer.disconnect();
+          observer.disconnect();
           callback(node);
         }
       });
@@ -20,7 +20,32 @@ function waitForVideo(callback: (video: HTMLVideoElement) => void) {
 
 waitForVideo((video) => {
   console.log(video);
+  console.log(video.currentTime);
 });
+
+interface Message {
+  type: "urlChanged";
+  url: string;
+}
+
+chrome.runtime.onMessage.addListener(
+  (
+    message: Message,
+    sender: chrome.runtime.MessageSender,
+    sendResponse: (response?: any) => void
+  ) => {
+    const currentUrl = window.location.href;
+
+    // console.log(currentUrl);
+
+    if (!message.url.includes("video")) return;
+
+    waitForVideo((video) => {
+      console.log(video);
+      console.log(video.currentTime);
+    });
+  }
+);
 
 // let videoEl: HTMLVideoElement;
 //
