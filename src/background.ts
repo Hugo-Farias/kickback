88edb0777cc0f58b8a70bfee01a015f44839eeb0
Hugo-Farias <hg80rod@gmail.com>
2023-05-13 21:24:02 -0300

    store function under construction

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,6 @@
 import { Message } from "./typeDef";
 
+// send message to content Scripts every time the url updates
 chrome.webNavigation.onHistoryStateUpdated.addListener(
   (details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) => {
     const tabId = details.tabId;
@@ -7,8 +8,8 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(
 
     if (url && !url.includes("kick.com/video")) return;
 
-    const message: Message = { type: "urlChanged", url: url };
+    // const message: Message = { type: "urlChanged", url: url };
     // setTimeout(() => chrome.tabs.sendMessage(tabId, message), 5000);
-    chrome.tabs.sendMessage(tabId, message);
+    chrome.tabs.sendMessage(tabId, "urlChanged");
   }
 );
--- src/videoObserver.ts
+++ src/videoObserver.ts
@@ -1,13 +1,13 @@
-import { Message } from "./typeDef";
-
 console.log("observer running");
 
+const url = window.location.href;
+const urlId = url.replace("https://kick.com/video/", ""); // Current url video ID
+const intervalSecs = 10; // Interval to store current time to local storage
+
 // Wait for video element
-function waitForVideo(
-  callback: (video: HTMLVideoElement, urlId: string) => void
-) {
-  const url = window.location.href;
+function waitForVideo(callback: (video: HTMLVideoElement) => void) {
   if (!url.includes("video")) return;
+
   const observer = new MutationObserver(() => {
     const videoElement = document.querySelector("video");
     if (videoElement && videoElement.readyState >= 3) {
@@ -24,15 +24,27 @@ function waitForVideo(
 
 // function to be called when video available
 const resume = function (videoEl: HTMLVideoElement) {
-  videoEl.currentTime = 1500;
+  videoEl.currentTime = 1000;
 };
 
+const storeTime = function (videoEL: HTMLVideoElement) {
+  const videoTime = videoEL.currentTime.toString();
+
+  console.log(JSON.stringify({ [urlId]: [videoTime] }));
+  localStorage.setItem("TimeStamps", JSON.stringify({ [urlId]: [videoTime] }));
+};
+
+// init
 waitForVideo((videoEl) => {
   resume(videoEl);
+
+  videoEl.addEventListener("play", () => {
+    setInterval(() => storeTime(videoEl), intervalSecs * 1000);
+  });
 });
 
-chrome.runtime.onMessage.addListener((message: Message) => {
-  if (!message.url.includes("video")) return;
+chrome.runtime.onMessage.addListener(() => {
+  if (!url.includes("video")) return;
 
   waitForVideo((videoEl) => {
     resume(videoEl);
