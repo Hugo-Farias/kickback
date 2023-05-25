77f69a895c83fc2d472a1a3f643d629485e60823
Hugo-Farias <hg80rod@gmail.com>
2023-05-25 17:36:52 -0300

    added tag for currently playing vod

--- src/helper.ts
+++ src/helper.ts
@@ -30,24 +30,6 @@ export const waitForElementList = function (
   }, timeoutDuration * 1000);
 };
 
-export const waitForElement = function (
-  el: string,
-  url: string | null = null,
-  check: string = "",
-  callback: (element: Element) => {}
-) {
-  if (url && !url.includes(check)) return;
-
-  const observer = new MutationObserver(() => {
-    const element: Element | null = document.querySelector(el);
-    if (element) {
-      observer.disconnect();
-      callback(element);
-    }
-  });
-
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
+export const getIdFromUrl = function (url: string): string {
+  return url.replace("https://kick.com/video/", "");
 };
--- src/progressBar.ts
+++ src/progressBar.ts
@@ -1,4 +1,4 @@
-import { waitForElementList } from "./helper";
+import { getIdFromUrl, waitForElementList } from "./helper";
 import { getData } from "./videoHelper";
 import { localStorageName, showProgressBarOnThumbs } from "./config";
 import { StoredStamps } from "./typeDef";
@@ -7,6 +7,7 @@ const data: StoredStamps = getData(localStorageName, false) as StoredStamps;
 
 const init = function () {
   if (!showProgressBarOnThumbs) return;
+  const currentId: string = getIdFromUrl(window.location.href);
 
   waitForElementList(
     ".grid-item > div > a[href^='/video/']",
@@ -14,7 +15,7 @@ const init = function () {
       if (!elArr) return null;
 
       elArr.forEach((v) => {
-        const id = v.href.replace("https://kick.com/video/", "");
+        const id = getIdFromUrl(v.href);
 
         if (!data.timestamps[id]) return;
 
@@ -23,17 +24,40 @@ const init = function () {
         const percentage = (currTime / totalTime) * 100;
 
         const div = document.createElement("div");
-        div.className = "progress-bar";
+
+        const color = "#53FC18";
+
+        // if (currentId === id) {
+        //   color = "#2f8110";
+        //   v.style.border = `1px solid ${color}`;
+        // }
+
         div.style.position = "absolute";
         div.style.height = "4px";
         div.style.width = "100%";
         div.style.bottom = "0";
-        div.style.background = `linear-gradient(to right, #53FC18 ${percentage}%, #9c9c9c 0)`;
+        div.style.background = `linear-gradient(to right, ${color} ${percentage}%, #9c9c9c 0)`;
 
         v.style.position = "relative";
         v.style.overflow = "show";
 
         v.appendChild(div);
+
+        if (currentId === id) {
+          const npDiv = document.createElement("div");
+
+          npDiv.className =
+            "right-2 top-2 rounded bg-black/80 px-1 text-sm font-light text-white";
+          npDiv.style.position = "absolute";
+          npDiv.textContent = "Now Playing";
+          // npDiv.style.paddingInline = "0.25rem";
+          // npDiv.style.height = "50px";
+          // npDiv.style.width = "100px";
+          // npDiv.style.backgroundColor = "rgba(0,0,0,0.4)";
+          // npDiv.style.top = "0";
+          // npDiv.style.right = "0";
+          v.appendChild(npDiv);
+        }
       });
     }
   );
