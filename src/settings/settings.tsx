7c6c4ccd7b9412e8a9c04913e07644b3cc65d5d0
Hugo-Farias <hg80rod@gmail.com>
2023-08-18 15:42:21 -0300

    dummy options in settings for reference

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -1,7 +1,41 @@
 import "./settings.scss";
 
+const dummyOptions: {
+  label: string;
+  type: "checkbox" | "number";
+  value: boolean | number;
+}[] = [
+  {
+    label: "Show progress bar on thumbnail previews",
+    type: "checkbox",
+    value: true,
+  },
+  {
+    label: "Skip forwards or back with arrow keys",
+    type: "checkbox",
+    value: true,
+  },
+  { label: "Skip amount in seconds", type: "number", value: 5 },
+];
+
 const Settings = function () {
-  return <h1 className="settings">Settings</h1>;
+  const optionsJSX = dummyOptions.map((v, i) => {
+    return (
+      <li key={i}>
+        <span>{v.label}</span>
+        <input type={v.type} />
+      </li>
+    );
+  });
+
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      <div className="options">
+        <ul>{optionsJSX}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
