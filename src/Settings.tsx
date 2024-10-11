4953e8dbb9312b867c4a6f1867589727bd3d946b
Hugo-Farias <hg80rod@gmail.com>
2024-10-10 21:59:59 -0300

    start: settings ui

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -36,7 +36,31 @@ const settingsValues: OptionsT[] = [
 const Settings = function () {
   console.log(settingsValues);
 
-  return <h1 className={"text-blue-500"}>Settings page</h1>;
+  return (
+    <div
+      className={
+        "font-inter flex h-dvh w-full flex-col items-center gap-6 bg-[#141517] p-10 text-[#fdfdfd]"
+      }
+    >
+      <h1 className={"text-3xl font-bold tracking-wide"}>Kickback Settings</h1>
+      <div
+        className={
+          "flex w-full flex-col justify-center rounded-md bg-white/10 py-10"
+        }
+      >
+        <div className={"mx-auto space-y-3 text-xl"}>
+          {settingsValues.map((value, index) => {
+            return (
+              <div className={"flex gap-6"}>
+                <input type={"checkbox"} aria-label={value.label} />
+                <label key={index}>{value.label}</label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
