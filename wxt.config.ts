71b0e20f238ead55c7190156b158cbb9f145c81d
Hugo-Farias <hg80rod@gmail.com>
2026-05-03 05:57:27 -0300

    add: readme markdown

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     name: "__MSG_extName__",
     description: "__MSG_extDescription__",
     default_locale: "en",
-    version: "3.0.2",
+    version: "3.0.3",
     permissions: ["storage"],
   },
 });
